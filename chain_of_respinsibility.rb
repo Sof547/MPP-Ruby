050e7a63aa5573d453c1086547e85911a0ed03cf
Sof547 <sonia20041967@gmail.com>
2023-12-08 18:38:45 +0200

    initial commit

--- chain_of_respinsibility.rb
+++ chain_of_respinsibility.rb
@@ -1,2 +1,73 @@
-# frozen_string_literal: true
+# Базовий клас обробника
+class AuthorizationHandler
+  attr_accessor :next_handler
 
+  def set_next_handler(handler)
+    @next_handler = handler
+  end
+
+  def authorize(request)
+    raise NotImplementedError, 'Subclasses must implement this method'
+  end
+
+  protected
+
+  def process_next_handler(request)
+    next_handler.nil? || next_handler.authorize(request)
+  end
+end
+
+# Конкретний обробник для перевірки електронної адреси
+class EmailHandler < AuthorizationHandler
+  def authorize(request)
+    if valid_email?(request[:email])
+      process_next_handler(request)
+    else
+      puts('Адреса повинна містити @')
+      false
+    end
+  end
+
+  private
+
+  def valid_email?(email)
+    email.include?('@')
+  end
+end
+
+# Конкретний обробник для перевірки паролю
+class PasswordHandler < AuthorizationHandler
+  def authorize(request)
+    if valid_password?(request[:password])
+      process_next_handler(request)
+    else
+      puts('Пароль повинен бути довжиною мінімум 8 символів')
+      false
+    end
+  end
+
+  private
+
+  def valid_password?(password)
+    password.length >= 8
+  end
+end
+
+# Використання ланцюжка обробників
+email_handler = EmailHandler.new
+password_handler = PasswordHandler.new
+
+email_handler.set_next_handler(password_handler)
+
+# Створення об'єкта запиту з інформацією користувача
+request = { email: 'user@gmail.com', password: '123456789' }
+
+# Виклик обробки запиту
+result = email_handler.authorize(request)
+
+# Перевірка результатів
+if result
+  puts 'Користувач успішно авторизований.'
+else
+  puts 'Авторизація не вдалася.'
+end
--- stack.rb
+++ stack.rb
@@ -1,2 +1,73 @@
-# frozen_string_literal: true
+class Node
+  attr_accessor :value, :prev
 
+  def initialize(value = nil, prev = nil)
+    @value = value
+    @prev = prev
+  end
+end
+
+class Stack
+  def initialize
+    @top = nil
+    @size = 0
+  end
+
+  def push(item)
+    new_node = Node.new(item, prev = @top)
+    @top = new_node
+    @size += 1
+  end
+
+  def pop
+    raise "Стек порожній" if empty?
+
+    popped_item = @top.value
+    @top = @top.prev
+    @size -= 1
+
+    popped_item
+  end
+
+  def peek
+    raise "Стек порожній" if empty?
+    @top.value
+  end
+
+  def empty?
+    @top.nil?
+  end
+
+  def size
+    @size
+  end
+
+  def to_s
+    current_node = @top
+    result = []
+    while current_node
+      result << current_node.value
+      current_node = current_node.prev
+    end
+    "[#{result.reverse.join(', ')}]"
+    end
+end
+
+stack = Stack.new
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(666)
+puts stack.to_s
+
+puts "Stack size: #{stack.size}"  # Stack size: 4
+puts "Top element: #{stack.peek}" # Top element: 666
+puts "Popped element: #{stack.pop}" # Popped element: 666
+puts stack.to_s
+puts "Is the stack empty? #{stack.empty?}" # Is the stack empty? false
+
+puts "Top element: #{stack.peek}" # Top element: 3
+
+stack.push(4)
+puts stack.to_s
